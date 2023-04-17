0854d1f12117bc72dfb364707dbe1ff1bfaeec13
sabato-nocera <nocera.sabato@gmail.com>
2023-04-17 12:41:21 +0200

    Add password

--- src/com/itsvil/Starter.java
+++ src/com/itsvil/Starter.java
@@ -5,6 +5,8 @@ import java.util.stream.IntStream;
 
 public class Starter {
     public static void main(String[] args) {
+        String password = "password";
+
         var randomSort = new RandomSort();
         final var MAX_VALUE = 100;
         final var ARRAY_SIZE = 10;
