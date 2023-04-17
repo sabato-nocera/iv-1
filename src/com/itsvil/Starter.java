1458e2b8b087c425c649024eb3212233d0356fb8
sabato-nocera <nocera.sabato@gmail.com>
2023-04-17 12:44:45 +0200

    Add n

--- src/com/itsvil/Starter.java
+++ src/com/itsvil/Starter.java
@@ -5,11 +5,11 @@ import java.util.stream.IntStream;
 
 public class Starter {
     public static void main(String[] args) {
-        String password = "password";
-
         var randomSort = new RandomSort();
         final var MAX_VALUE = 100;
         final var ARRAY_SIZE = 10;
+
+        var n = new Random().nextInt(MAX_VALUE);
 
         int[] array = IntStream.generate(()-> new Random().nextInt(MAX_VALUE)).limit(ARRAY_SIZE).toArray();
 
